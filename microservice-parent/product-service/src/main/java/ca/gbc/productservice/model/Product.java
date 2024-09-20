e959b0541525dc9b28112b51d2695b8e1227c179
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2024-09-20 09:29:45 -0400

    new 2nd week

--- microservice-parent/product-service/src/main/java/ca/gbc/productservice/model/Product.java
+++ microservice-parent/product-service/src/main/java/ca/gbc/productservice/model/Product.java
@@ -1,7 +1,15 @@
 package ca.gbc.productservice.model;
 import lombok.Data;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(value = "document")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
     private String id;
     private String name;
