17fdc272bdf386149ba90cc20839f750f01e2269
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2024-10-04 10:28:55 -0400

    Week5 Friday

--- microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -1,33 +1,34 @@
 package ca.gbc.productservice;
 
 import io.restassured.RestAssured;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
-//tell springs boot to look for main config class (@springbootaplication)
+// Tell Springboot ot look for a main configuration class (@springbootApplication)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
-    //this annotation is used in combination with testcontained to automatically configure the connecttion to
-    // the test mongodbcontainer
+
+    // This annotation is used in combination with TestContainers to
+    // automatically configure the connection to the test mongodb
+    @ServiceConnection
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
     @LocalServerPort
     private Integer port;
-    //http://localhost:port/api/product
 
     @BeforeEach
     void setup(){
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port=port;
+        RestAssured.port = port;
     }
 
-    static {
+    static{
         mongoDBContainer.start();
     }
 
@@ -35,13 +36,12 @@ class ProductServiceApplicationTests {
     void createProductTest(){
         String requestBody = """
                 {
-                    "name" : "Samsung TV",
-                    "description" : "Samsung TV - Model 2024",
-                    "price" : "2000";   
-                }            
+                 "name":"Samsung TV",
+                 "description" : "Samsung TV - Model 2024",
+                 "price" : "2000"
+                }
                 """;
-        //BDD = 0 Behavioural driven development (given, when, then)
-
+        //BDD -0  Behavioural Driven Development
         RestAssured.given()
                 .contentType("application/json")
                 .body(requestBody)
@@ -51,23 +51,22 @@ class ProductServiceApplicationTests {
                 .log().all()
                 .statusCode(201)
                 .body("id", Matchers.notNullValue())
-                .body("name", Matchers.equalTo("Samsung TV"))
-                .body("description", Matchers.equalTo("Samsung TV - Model 2024"))
-                .body("price", Matchers.equalTo(2000));
-
+                .body("name",Matchers.equalTo("Samsung TV"))
+                .body("description",Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("price",Matchers.equalTo(2000));
     }
 
     @Test
     void getAllProductsTest(){
         String requestBody = """
                 {
-                    "name" : "Samsung TV",
-                    "description" : "Samsung TV - Model 2024",
-                    "price" : "2000";   
-                }            
+                    "name":"Samsung TV",
+                    "description":"Samsung LCD TV 2016 Model",
+                    "price":"2000"
+                }
                 """;
-        //BDD = 0 Behavioural driven development (given, when, then)
 
+        //BDD - 0 Behavioural Driven Development(Given, When, Then)
         RestAssured.given()
                 .contentType("application/json")
                 .body(requestBody)
@@ -78,20 +77,21 @@ class ProductServiceApplicationTests {
                 .statusCode(201)
                 .body("id", Matchers.notNullValue())
                 .body("name", Matchers.equalTo("Samsung TV"))
-                .body("description", Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("description", Matchers.equalTo("Samsung LCD TV 2016 Model"))
                 .body("price", Matchers.equalTo(2000));
 
         RestAssured.given()
                 .contentType("application/json")
                 .when()
-                .get()
+                .get("/api/product")
                 .then()
-                .log().all().
-                statusCode(200)
+                .log().all()
+                .statusCode(200)
                 .body("size()", Matchers.greaterThan(0))
                 .body("[0].name", Matchers.equalTo("Samsung TV"))
-                .body("[0].description", Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("[0].description", Matchers.equalTo("Samsung LCD TV 2016 Model"))
                 .body("[0].price", Matchers.equalTo(2000));
-    }
-}
 
+    }
+
+}
